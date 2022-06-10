025260e0163649251c48d42ba25852113ee8dd48
idanielarias <ariasdaniel@hotmail.com>
2022-06-10 14:46:36 -0300

    RGroups

---
+++ Solution/main.tf
@@ -0,0 +1,44 @@
+//Backend Example.
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraform"
+    storage_account_name = "hackterraform"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
+  required_providers {
+    azurerm = {
+      version = "~> 2.19"
+    }
+  }
+}
+
+//Provider example
+provider "azurerm" {
+  features {}
+}
+
+//RG
+module "RGroups" {
+  source = "./Modules/RGroups"
+  tupla_rgname_lc = var.tupla_rgname_lc
+}
+
+//Log Analytics
+module "LogAnalitycs" {
+  source                = "./Modules/LogAnalitycs"
+  name                  = "mshack"
+  depends_on            = [module.RGroups] // Dependencia Explicita.
+  resource_group_name   = join("," , module.RGroups.name[*].RGEU2001.name) // Dependencia implicita
+  location              = join("," , module.RGroups.name[*].RGEU2001.location) // Dependencia implicita
+  sku                   = "Free"
+  retention_in_days     = 7
+  tags = merge(local.common_tags, local.extra_tags)
+  solutions = [
+        {
+            solution_name = "AzureActivity",
+            publisher = "Microsoft",
+            product = "OMSGallery/AzureActivity",
+        },
+    ]
+}
